b8b1edd8e3d6bfe9f2969e2d1f1436b91e389cb4
Venkkyyy <vinikeshhiranandani@gmail.com>
2026-07-05 00:05:13 +0530

    feat: configure Firebase integration and update Android build settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.maruti.stationery.maruti_stationery"
+    namespace = "com.example.maruti_stationery"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.maruti.stationery.maruti_stationery"
+        applicationId = "com.example.maruti_stationery"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
